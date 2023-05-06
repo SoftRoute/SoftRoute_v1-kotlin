0d2832914e9d5b2721430175bf8f50aece04bf47
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2023-05-05 23:57:54 -0500

    CardInActivity

--- app/src/main/java/com/example/softroute_v1/admin/ViewCommentsActivity.kt
+++ app/src/main/java/com/example/softroute_v1/admin/ViewCommentsActivity.kt
@@ -2,6 +2,8 @@ package com.example.softroute_v1.admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.softroute_v1.R
 
 class ViewCommentsActivity : AppCompatActivity() {
@@ -9,5 +11,11 @@ class ViewCommentsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_comments)
 
+        val commentView = layoutInflater.inflate(R.layout.comment_card, null)
+        val cardView = commentView.findViewById<CardView>(R.id.cardComment)
+
+        // Agrega el cardView a la vista principal
+        //val mainLayout = findViewById<ConstraintLayout>()
+        //mainLayout.addView(commentView)
     }
 }
