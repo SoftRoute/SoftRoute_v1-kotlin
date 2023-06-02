4b183203d4e2ef0e6e0a1dcd60731b4b606951c0
Cristhian Orosco <u201921031@upc.edu.pe>
2023-06-02 01:45:08 -0500

    feat(Document): add adapter, holder y complete documentService

---
+++ app/src/main/java/com/example/softroute_v1/controller/retrofitApiConsume/Documents/holder/DocumentViewHolder.java
@@ -0,0 +1,9 @@
+package com.example.softroute_v1.controller.retrofitApiConsume.Documents.holder;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+class DocumentViewHolder{
+
+}
--- app/src/main/java/com/example/softroute_v1/controller/retrofitApiConsume/Documents/service/DocumentApiService.kt
+++ app/src/main/java/com/example/softroute_v1/controller/retrofitApiConsume/Documents/service/DocumentApiService.kt
@@ -3,8 +3,10 @@ package com.example.softroute_v1.controller.retrofitApiConsume.Documents.service
 import com.example.softroute_v1.controller.retrofitApiConsume.Documents.model.Document
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface DocumentApiService {
@@ -16,5 +18,11 @@ interface DocumentApiService {
 
     @POST("documents")
     suspend fun createDocument(@Body document: Document): Response<Document>
+
+    @PUT("documents/{id}")
+    suspend fun updateDocument(@Path("id") id:String, @Body document: Document): Response<Document>
+
+    @DELETE("documents/{id}")
+    suspend fun deleteDocument(@Path("id") id:String): Response<Document>
 
 }
