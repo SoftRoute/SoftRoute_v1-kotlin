16f770a5dafddf3491112bb759705783fa15911b
YeniAlvarado <u20201b032@upc.edu.pe>
2023-06-04 18:14:41 -0500

    Add new components

--- app/src/main/java/com/example/softroute_v1/controller/fragments/admin/AdminAddShipmentFragment.kt
+++ app/src/main/java/com/example/softroute_v1/controller/fragments/admin/AdminAddShipmentFragment.kt
@@ -51,7 +51,6 @@ class AdminAddShipmentFragment : Fragment() {
 
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_admin_add_shipment, container, false)
-
         //TYPE PACKAGE CONFI
         autoCompletedText = view.findViewById(R.id.spinner)
 
--- app/src/main/java/com/example/softroute_v1/controller/fragments/admin/AdminFreights.kt
+++ app/src/main/java/com/example/softroute_v1/controller/fragments/admin/AdminFreights.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import com.example.softroute_v1.R
 
+
 class AdminFreights: Fragment() {
+
+    private lateinit var TypePayment: Spinner
+    private lateinit var ShipmentType: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,6 +25,16 @@ class AdminFreights: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_freights, container, false)
+        val view =  inflater.inflate(R.layout.fragment_admin_freights, container, false)
+        TypePayment = view.findViewById(R.id.PaymentType)
+        val values = arrayOf("Visa", "Contado", "Contraentrega")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, values)
+        TypePayment.setAdapter(adapter)
+
+        ShipmentType = view.findViewById(R.id.ShipmentType)
+        val Shipmentvalues = arrayOf("electronico", "libros", "comida")
+        val Shipmentadapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, Shipmentvalues)
+        ShipmentType.setAdapter(Shipmentadapter)
+        return view
     }
 }
